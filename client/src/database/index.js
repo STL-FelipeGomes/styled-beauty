07cda0c68ca525eae04b95a84603513c9ee870b7
STL-FelipeGomes <felipegomesoliveira12@gmail.com>
2022-12-10 13:36:29 -0300

    integration whit firebase front

---
+++ client/src/database/index.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { getAuth } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './config';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth;
